af37cb68f3444f718b7a487c9ee8b54c6c3ba058
phelian23 <femikola23@gmail.com>
2022-02-15 15:58:58 +0400

    Create author class

---
+++ classes/author.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+# Author class
+class Author
+  attr_accessor :first_name, :last_name
+  attr_reader :items
+
+  def initialize(first_name, last_name)
+    @id = rand(1..1000)
+    @first_name = first_name
+    @last_name = last_name
+    @items = []
+  end
+
+  def add_item(item)
+    item.author = self
+    @items << item unless @items.include?(item)
+  end
+end

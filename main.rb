c6b14520a5055072dc1e48abb57ee391d4c01405
Timo <timowester@gmail.com>
2022-02-15 10:07:49 +0100

    Add main.rb

---
+++ main.rb
@@ -0,0 +1,69 @@
+class App
+  def run
+    puts 'Welcome!'
+    loop do
+      menu
+      option = gets.chomp
+      break if option == '13'
+
+      get_num option
+    end
+    puts 'Thank you for using our Library!'
+  end
+
+  def menu
+    puts 'Please choose an option by entering a number'
+    puts '1 - List all books'
+    puts '2 - List all music albums'
+    puts '3 - List all movies'
+    puts '4 - List all games'
+    puts '5 - List all genres'
+    puts '6 - List all labels'
+    puts '7 - List all authors'
+    puts '8 - List all sources'
+    puts '9 - Add a book'
+    puts '10 - Add a music album'
+    puts '11 - Add a movie'
+    puts '12 - Add a game'
+    puts '13 - Exit'
+  end
+
+  def get_num(option)
+    case option
+    when '1'
+      list_all_books
+    when '2'
+      list_all_people
+    when '3'
+      create_person
+    when '4'
+      create_book
+    when '5'
+      create_rental
+    when '6'
+      list_rentals_by_person_id
+    else
+      puts 'enter a number between 1 and 7'
+    end
+  end
+
+  def create_person
+    print 'Do you want to create a student (1) or teacher (2) [Input a number]: '
+    option = gets.chomp
+
+    case option
+    when '1'
+      create_student
+    when '2'
+      create_teacher
+    else
+      puts 'Invalid input. Kindly type 1 or 2'
+    end
+  end
+
+def main
+  app = App.new
+  app.run
+end
+main
+end

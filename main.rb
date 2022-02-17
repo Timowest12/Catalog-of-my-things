16de5d09b68dddc6c8171f07b9e7a6ab9d19f5b9
King Josaphat Chewa <26507383+Kingjosh007@users.noreply.github.com>
2022-02-17 11:58:14 +0100

    Make menu prettier

--- main.rb
+++ main.rb
@@ -35,20 +35,26 @@ class App
   end
 
   def menu
-    puts 'Please choose an option by entering a number'
-    puts '1 - List all books'
-    puts '2 - List all music albums'
-    puts '3 - List all movies'
-    puts '4 - List all games'
-    puts '5 - List all genres'
-    puts '6 - List all labels'
-    puts '7 - List all authors'
-    puts '8 - List all sources'
-    puts '9 - Add a book'
-    puts '10 - Add a music album'
-    puts '11 - Add a movie'
-    puts '12 - Add a game'
-    puts '13 - Exit'
+    puts `clear`
+    puts "\n\n\n\t\t ---------------------------".bold.on_magenta
+    puts "\t\t|  CATALOG OF MY THINGS  |".bold.on_magenta
+    puts "\t\t ---------------------------".bold.on_magenta
+    puts "\t____________________________________________________".magenta
+    puts "\n\t Please choose an option by entering a number"
+    puts "\n\t\t #{' 1 '.brown.on_magenta} - List all books\n"
+    puts "\n\t\t #{' 2 '.brown.on_magenta} - List all music albums \n"
+    puts "\n\t\t #{' 3 '.brown.on_magenta} - List all movies\n"
+    puts "\n\t\t #{' 4 '.brown.on_magenta} - List all games\n"
+    puts "\n\t\t #{' 5 '.brown.on_magenta} - List all genres\n"
+    puts "\n\t\t #{' 6 '.brown.on_magenta} - List all labels\n"
+    puts "\n\t\t #{' 7 '.brown.on_magenta} - List all authors\n"
+    puts "\n\t\t #{' 8 '.brown.on_magenta} - List all sources\n"
+    puts "\n\t\t #{' 9 '.brown.on_magenta} - Add a book\n"
+    puts "\n\t\t #{' 10 '.brown.on_magenta} - Add a music album\n"
+    puts "\n\t\t #{' 11 '.brown.on_magenta} - Add a movie\n"
+    puts "\n\t\t #{' 12 '.brown.on_magenta} - Add a game\n"
+    puts "\n\t\t #{' 13 '.brown.on_magenta} - #{' Exit '.bold.on_red}\n\t #{'_'.magenta * 53} \n\n"
+    print "\t\t  "
   end
 
   def get_num(option)

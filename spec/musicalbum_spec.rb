03e8e1e65d781f3b5d339003549eb94a1c6205fc
Timo <timowester@gmail.com>
2022-02-18 09:41:20 +0100

    Add musicalbum tests

--- spec/musicalbum_spec.rb
+++ spec/musicalbum_spec.rb
@@ -3,18 +3,19 @@ require_relative '../classes/musicalbum'
 
 describe MusicAlbum do
   before :each do
-    @musictest = MusicAlbum.new(2002-01-01,true,true)
+    @musictest = MusicAlbum.new(2002-01-01,true,true,true)
   end
 
   it 'music should be an instance of Music class' do
     expect(@musictest).to be_an_instance_of MusicAlbum
   end
 
-  it 'expect publish date to be a date' do
-    expect(@musictest.publish_date).to be_a(Date)
+  it 'expect publish date to be correct' do
+    expect(@musictest.publish_date).to be(2002-01-01)
+  end
+  
+  it 'expect on spotify to be correct' do
+    expect(@musictest.on_spotify).to be(true)
   end
 
-  it 'check if on_spotify works correctly' do
-    expect(@musictest.on_spotify?).to be true
-  end
 end

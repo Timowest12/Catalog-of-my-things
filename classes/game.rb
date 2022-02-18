305fafbf83f1a70fef132bcedf0144d77638f77f
King Josaphat Chewa <26507383+Kingjosh007@users.noreply.github.com>
2022-02-18 06:33:54 +0100

    Update Game class: constructor and method

--- classes/game.rb
+++ classes/game.rb
@@ -6,13 +6,15 @@ require_relative 'item'
 class Game < Item
   attr_accessor :multiplayer, :last_played_at, :publish_date
 
-  def initialize(multiplayer, last_played_at, publish_date)
-    super(publish_date)
-    @multiplayer = multiplayer
+  def initialize(publish_date, last_played_at, multiplayer = true, archived = false, my_id = rand(1..10_000))
+    super(publish_date, archived, my_id)
     @last_played_at = last_played_at
+    @multiplayer = multiplayer
   end
 
   def can_be_archived?
-    super() && (@last_played_at < (Time.now - (2 * 365.25 * 24 * 60 * 60)))
+    nb_secs_in_year = 365.25 * 24 * 60 * 60
+    diff_in_years = ((Date.today.to_time - @last_played_at.to_time) / nb_secs_in_year)
+    super && diff_in_years > 2
   end
 end

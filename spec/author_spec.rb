20cf1fc79cf1e683430aee45653c0dcf9c257469
King Josaphat Chewa <26507383+Kingjosh007@users.noreply.github.com>
2022-02-18 15:59:56 +0100

    Write a test for add_item method in genre_spec.rb

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -3,7 +3,7 @@ require_relative '../classes/author'
 
 describe Author do
   context 'When testing the Author class' do
-    it 'The add_items method should add items to @@items' do
+    it 'The add_items method should add item to @@items' do
         author = Author.new('John', 'Doe')
         item1 = Item.new(Date.today.to_s, true);
         
--- spec/genre_spec.rb
+++ spec/genre_spec.rb
@@ -12,4 +12,11 @@ describe Genre do
   it 'genre name should be correct' do
     @name == 'country'
   end
+
+  it 'The add_items method should add item to @@items' do
+    item1 = Item.new(Date.today.to_s, true);
+    @genre.add_item(item1)
+    items = Genre.class_variable_get(:@@items)
+    expect(items).to include(item1)
+  end
 end
